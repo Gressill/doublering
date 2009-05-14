630f00788f3ef4a10e614ab8dc7321193970b490
abelfly.zhaofei <abelfly.zhaofei@56f4bcca-3f5e-11de-90e1-f9f1724fc9cc>
2009-05-14 13:58:16 +0000

    Member中添加几个私有变量保持解析出来的用户信息。 Commomname,place,username，将之保持在java的map中 二、添加showparse()函数完成显示。 三、parse功能统一命名方式：parse+字段名(TagNode tag)方式。 第五步 建包util/Constant.java，设置数据库基本连接参数。 在基类Html.java中实现插入数据insertSQL函数，参数是： Tablename: string 表名 Idname: string  字段名 Idvalue: string  字段值 Insertsql: string  执行的SQL语句 函数先检查给定字段是否已经在被数据库抓取，如果没抓取，则执行SQL插入数据库。

--- src/parser/Html.java
+++ src/parser/Html.java
@@ -1,36 +1,50 @@
 package parser;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.htmlparser.Parser;
 import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
+
+import db.Dbo;
 
 public class Html {
 	/**
 	 * the parserstr
 	 * 
 	 */
-	private String parseStr;
+	private String _url;
+	public static Dbo dbo;
 	/**
 	 * 
 	 * construct function
 	 * 
 	 */
-	public Html(String str){	
-		parseStr = str;
+	public Html(String str){
+		_url = str;
+		try{
+		dbo = new Dbo();
+		if(dbo.OpenConnection()){
+			System.out.println("database connected");
+		}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	/**
 	 * parent value return function
 	 * @return parseStr
 	 */
 	public String getPs(){
-		return parseStr;
+		return _url;
 	}
 	/**
 	 * parent value set function
 	 * @param string
 	 */
 	public void setPs(String str){
-		parseStr = str;
+		_url = str;
 	}
 	/**
 	 * render function: 基本的解析函数
@@ -39,12 +53,23 @@ public class Html {
 	public void render(){
 		try{
 			
-			Parser parser = new Parser(parseStr);
+			Parser parser = new Parser(_url);
 			NodeList list = parser.parse (null);
             System.out.println (list.toHtml());
 		}
 		catch(ParserException pe){
 			pe.printStackTrace();
+		}
+		
+	}
+	
+	public void inserSQL(String Tablename, String Idname, String Idvalue, String sql)
+	{
+		ResultSet res;
+		String testUniqueSQL = "SELECT count(*) AS res_num FROM `" + Tablename + "` WHERE `" + Idname + "`='"+ Idvalue +"'";
+		System.out.println(testUniqueSQL);
+		if(dbo.OpenConnection()){
+			int i = dbo.ExecuteUpdate(sql);
 		}
 		
 	}
---
+++ src/util/Constant.java
@@ -0,0 +1,7 @@
+package util;
+import java.util.Queue;
+public class Constant {
+	public final static String DB__DATABASE = "doublering";
+	public final static String DB_USER_NAME = "ctottrunsql";
+	public final static String DB_PASSWORD  = "ctottruningsql";
+}
