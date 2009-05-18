362953fd7b3bc6f3a0599750eb6e915ffe480f8b
abelfly.zhaofei <abelfly.zhaofei@56f4bcca-3f5e-11de-90e1-f9f1724fc9cc>
2009-05-18 13:21:04 +0000

    重新编写解析用户数据方法
    调用豆瓣的api来实现 
    
    主要处理代码在PeopleHandler.java 里面

---
+++ src/hander/PeopleHandler.java
@@ -0,0 +1,125 @@
+package hander;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import db.Dbo;
+
+public class PeopleHandler extends DefaultHandler {//继承DefaultHandler重写startElement解析XML
+
+	private Hashtable<String, String> _pentry;
+	private String cur_qName;
+	private Dbo db;
+	
+	public PeopleHandler(){
+		
+		try{
+			db = new Dbo();
+			if(db.OpenConnection()){
+				System.out.println("[System Info] Database connected.");
+			}
+		}catch(Exception e){
+				e.printStackTrace();
+		}
+		
+		 _pentry = new Hashtable<String, String>();
+		 clear();
+		 
+		System.out.println("[System info] Construction");
+	}
+	public void startElement(String uri, String localName, String qName,
+                             org.xml.sax.Attributes attributes) throws
+            SAXException {
+		cur_qName = qName;
+    	
+		//System.out.println("[System info]"+qName);
+		
+    	if ("id".equals(qName)){
+    		cur_qName = "id";
+    	}else if("title".equals(qName)){
+    		cur_qName = "title";
+    	}else if("link".equals(qName)){
+    		cur_qName = "link";
+    		if("alternate".equals(attributes.getValue("rel"))){
+    			String myhref = attributes.getValue("href");
+    			_pentry.put("douban_link", new String(myhref));
+    		}
+    		if("self".equals(attributes.getValue("rel"))){
+    			String myhref = attributes.getValue("href");
+    			String doubanid = myhref.substring(myhref.length()-7, myhref.length());
+    			_pentry.put("douban_id", new String(doubanid));
+    		}
+    	}else if("content".equals(qName)){
+    		cur_qName = "content";
+    	}else if("db:location".equals(qName)){
+    		cur_qName = "location";
+    		String wid = attributes.getValue("id");
+    		_pentry.put("location_id", wid);
+    	}else if("db:uid".equals(qName)){
+    		cur_qName = "uid";
+    	}
+        
+        super.startElement(uri, localName, qName, attributes);//递归调用
+
+    }
+    
+    public void characters(char[] ch,
+            int start,
+            int length){
+    	String text=new String(ch,start,length);
+    	if(text == ""){
+    		return;
+    	}
+    	Object o = _pentry.get(cur_qName);
+    	if(o != null){
+    		_pentry.put(cur_qName, new String(o.toString()+text));
+    	}else{
+    		_pentry.put(cur_qName, new String(text));
+    	}
+        //System.out.println(_pentry.toString());
+    }
+    
+    public void endDocument(){
+    	//Insert into the database here.
+    	store();
+    	clear();
+    }
+    private void store(){
+    	System.out.println(_pentry.toString());
+    	
+    	try{
+    		if(_pentry.get("uid") == null) return; //空的table，不储蓄
+    		String sqlInsert = "INSERT INTO `dr_user`(`uid`,`title`,`douban_id`,`douban_link`,`location_id`,`location`,`content`) VALUES ('"+
+    		_pentry.get("uid").toString()+"','"+_pentry.get("title").toString()+"','"+_pentry.get("douban_id").toString()+"','"+_pentry.get("douban_link").toString()+"','"+_pentry.get("location_id").toString()+"','"+_pentry.get("location").toString()+"','"+_pentry.get("content").toString()+"')";
+    		System.out.println(sqlInsert);
+    		if(db.OpenConnection()){
+    			db.ExecuteUpdate(sqlInsert);
+    		}
+    		}catch(Exception e){
+    			e.printStackTrace();
+    		}
+    }
+    private void clear(){
+    	_pentry.clear();
+    	_pentry.put("uid", "");
+    	_pentry.put("title", "");
+    	_pentry.put("douban_id", "");
+    	_pentry.put("douban_link", "");
+    	_pentry.put("location_id", "");
+    	_pentry.put("location", "");
+    	_pentry.put("content", "");
+    	//System.out.println(_pentry);
+    	//_pentry = new Hashtable<String, String>();
+//    	for(Iterator it = _pentry.keySet().iterator();it.hasNext(); )
+//    	{  
+//            String key = (String) it.next();
+//            System.out.println(key);
+//            _pentry.remove(key);
+//            _pentry.put(key.toString(),""); 
+//    	}
+    }
+
+}
