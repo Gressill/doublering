22950f7041f5b0217590f7d6509bea9c8ec8c567
abelfly.zhaofei <abelfly.zhaofei@56f4bcca-3f5e-11de-90e1-f9f1724fc9cc>
2009-05-19 13:59:17 +0000

    创建解析Book的parser

--- trunk/src/Doublering.java
+++ trunk/src/Doublering.java
@@ -13,6 +13,6 @@ public class Doublering {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub}
 		new Parserthd("people").start();//开启用户抓取进程
-		new Parserthd("subject").start();//开启subject抓取进程
+		//new Parserthd("book").start();//开启subject抓取进程
 	}
 }
