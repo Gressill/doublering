f06d6fd3e94d1fcf4433e032f1c26e46c16f1754
abelfly.zhaofei <abelfly.zhaofei@56f4bcca-3f5e-11de-90e1-f9f1724fc9cc>
2009-05-24 14:07:39 +0000

    修正停止bug

--- trunk/src/parser/Peoplefriendsparser.java
+++ trunk/src/parser/Peoplefriendsparser.java
@@ -52,23 +52,26 @@ public class Peoplefriendsparser  extends Service{
 	}
 	
 	public int parse(){
-		System.out.println("[System Info] Spiding people's friends");
-		int per_page_read=0;
+		
 		//每读取一个entry数据，per_read增加1,当每页读取完后，如果per_page_read<max_result，这表明数据用户好友信息被读完
 		int start_index=1;
-		int max_result =5;
+		int max_result =50;
 		try {
 			String url = Namespaces.userURL + "/" + uid  + "/friends";
 			DoubanQuery query = new DoubanQuery(new URL(url));
 			do{
+			System.out.println("[System Info] Spiding people's friends with {start_index: "+start_index+"} and {max_result: "+max_result+"}");	
+		
 			query.setStartIndex(start_index);
 			query.setMaxResults(max_result);
 			UserFeed uf = getFeed(query, UserFeed.class);
+			
+			
 			//UserFeed uf = myService.getUserFriends("1000001", 1, 2);
 			//List<UserEntry> ul = ;
-			String uid="",title="",douban_id="",douban_link="",location_id="",location="",content="";
+			String douban_uid="",title="",douban_id="",douban_link="",location_id="",location="",content="";
 			for(UserEntry u : uf.getEntries()){
-				per_page_read++;//表明当前是否
+				
 				for(Link l : u.getLinks()){
 					
 					if("alternate".equals(l.getRel())){
@@ -77,27 +80,43 @@ public class Peoplefriendsparser  extends Service{
 						douban_link = u.getLinks().get(1).getHref();
 						//获取豆瓣uid
 						if(douban_link.length()>20){
-						uid = douban_link.substring(douban_link.lastIndexOf("people")+7, douban_link.length()-1);
+						douban_uid = douban_link.substring(douban_link.lastIndexOf("people")+7, douban_link.length()-1);
 						}
 					}
 				}
 				
 				//获取豆瓣title
 				title = u.getTitle().getPlainText();
+				//过滤不安全数据
+				if(title !=null){
+					title = title.replace("'", "\\'");
+				}
 				//获取id,douban_id
 				String id = u.getId();
 				douban_id = id.substring(id.length()-7, id.length());
 				
+				System.out.println("[System Info] Parse ans spiding friend "+douban_id);
 				//获取location_id
 				location_id = u.getLocation_id();
+				//过滤不安全数据
+				if(location_id != null){
+					location_id = location_id.replace("'", "\\'");
+				}
 				//获取位置
 				location=u.getLocation();
+				//过滤不安全数据
+				if(location !=null){
+					location = location.replace("'", "\\'");
+				}
 				//获取content
 				content = u.getPlainTextContent();
-				
+				//过滤不安全数据
+				if(content !=null){
+					content = content.replace("'", "\\'");
+				}
 				//将entry信息插入user表
 				String insert_user_sql = "INSERT INTO `dr_user` (`uid`,`title`,`douban_id`,`douban_link`,`location_id`,`location`,`content`) VALUES ("
-					+"'"+uid+"',"
+					+"'"+douban_uid+"',"
 					+"'"+title+"',"
 					+"'"+douban_id+"',"
 					+"'"+douban_link+"',"
@@ -106,24 +125,25 @@ public class Peoplefriendsparser  extends Service{
 					+"'"+content+"')";
 				store_sql(insert_user_sql);
 				
-				if( ! "".equals(douban_id)){
+				if( "".equals(douban_id) == false){
 				//将关系插入user_friends表
-				String insert_friends_sql = "INSERT INTO `dr_user_friends` (`userid`,`friendsid`) VALUES ('"+uid+"','"+douban_id+"')";
+				String insert_friends_sql = "INSERT INTO `dr_user_friends` (`userid`,`friendid`) VALUES ('"+uid+"','"+douban_id+"')";
 				store_sql(insert_friends_sql);
 				}
 			}
 			
 			
-			//如果per_page_read<max_result，则另外选择一个种子返回
-			if(per_page_read<max_result){
-				if("".equals(douban_id)){
+			if(uf.getTotalResults() <uf.getStartIndex() + max_result){
+				if("".equals(douban_id) == false){
 					return (int)Constant.min_id + (int)Math.random() * (Constant.max_id - Constant.min_id);
 				}
-				else
-					return (int)Integer.valueOf(douban_id);
+				else{
+					int ret =  (int)Integer.valueOf(douban_id);
+ 					return ret;
+				}
 			}
 			//否则，增加起始访问指标
-			start_index = start_index + max_result;
+			start_index = uf.getStartIndex() + max_result;
 			//停止1.5秒
 			Thread.sleep(2550);
 			}while(true);
