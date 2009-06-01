86ff2d9ebb18970819dcb73d7280e00cfd334491
abelfly.zhaofei <abelfly.zhaofei@56f4bcca-3f5e-11de-90e1-f9f1724fc9cc>
2009-06-01 05:10:48 +0000

    peopleparser的数据库部分

--- trunk/doc/collection.sql
+++ trunk/doc/collection.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS `dr_user_collection` (
   `status` varchar(20) NOT NULL,
   PRIMARY KEY  (`cid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='用户收藏表' AUTO_INCREMENT=1 ;
+
+
+CREATE TABLE IF NOT EXISTS `dr_user_friends` (
+  `ufid` int(11) NOT NULL auto_increment,
+  `userid` int(11) default '0',
+  `friendid` int(11) default '0',
+  PRIMARY KEY  (`ufid`),
+  UNIQUE KEY `userid` (`userid`,`friendid`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='用户关系表' AUTO_INCREMENT=706 ;
